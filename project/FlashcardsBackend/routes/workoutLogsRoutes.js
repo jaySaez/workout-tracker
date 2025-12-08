6181e9f561ec67f62847f00103cfe10fab0932e4
jaySaez <jasonjoelsaez@gmail.com>
2025-12-08 00:18:14 -0500

    Updated routes

--- project/FlashcardsBackend/routes/workoutLogsRoutes.js
+++ project/FlashcardsBackend/routes/workoutLogsRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addWorkoutLogs, getWorkoutLogs, deleteWorkoutLog } from '../controllers/workoutLogsController.js';
+import { addWorkoutLogs, getWorkoutLogs, deleteWorkoutLog, getWorkoutLogsById } from '../controllers/workoutLogsController.js';
 
 const router = express.Router();
 
 router.post('/', addWorkoutLogs);
 router.delete('/:workoutId', deleteWorkoutLog);
+router.get('/:workoutId', getWorkoutLogsById);
 router.get('/', getWorkoutLogs);
 
 export default router;
