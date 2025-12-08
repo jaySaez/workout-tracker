bbf551218aef17e5fb38bb2415052e356fdf61df
jaySaez <jasonjoelsaez@gmail.com>
2025-12-08 00:42:08 -0500

    Router fix

--- project/FlashcardsBackend/routes/workoutsRoutes.js
+++ project/FlashcardsBackend/routes/workoutsRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addworkouts, deleteWorkout, getWorkouts, toggleFavorite, getFavorites } from '../controllers/workoutsController.js';
+import { addworkouts, deleteWorkout, getWorkouts, toggleFavorite, getFavorites, getWorkoutById } from '../controllers/workoutsController.js';
 
 const router = express.Router();
 
 router.post('/', addworkouts);
 router.delete('/:workoutId', deleteWorkout);
 router.get('/', getWorkouts);
+router.get('/:workoutId', getWorkoutById)
 router.patch('/:workoutId/favorite', toggleFavorite);
 router.get('/favorites', getFavorites);
 
