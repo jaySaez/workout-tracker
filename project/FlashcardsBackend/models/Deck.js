a09353a34db9c02c9c4dc3a3ce025ae12eb62263
jaySaez <jasonjoelsaez@gmail.com>
2025-12-09 17:51:49 -0500

    Refactored leftover frontend names.

--- project/FlashcardsBackend/models/Deck.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const deckSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-});
-
-export const Deck = mongoose.model('Deck', deckSchema);
